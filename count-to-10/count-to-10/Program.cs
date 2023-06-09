c5c241fba7b374c50d75ae3e301a89467a2b61c4
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2023-06-09 18:45:11 +0100

    update
    this was a update lol
    add a new method call history
    and double to keep idea of number people put in

--- count-to-10/count-to-10/Program.cs
+++ count-to-10/count-to-10/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 double Set, count, total;
-double[] History; 
-Set = Convert.ToDouble( Console.ReadLine());
-History
+int keephistory =0;
+double[] History = {0,0,0,0,0};  
+Set = Convert.ToDouble( Console.ReadLine()); // this need to go into a loop
+/// this is to keep track of 5 pass number
+history();
+
+
+void history()
+{
+	History[keephistory] = Set;
+} 
