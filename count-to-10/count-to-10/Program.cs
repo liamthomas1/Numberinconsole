b208b5cf942f9ea274f8908679c11cd938b45af9
liamthomas1 <55701056+liamthomas1@users.noreply.github.com>
2023-06-09 18:27:00 +0100

    setting up the code
    this is me getting the folder in the right place so yeah

---
+++ count-to-10/count-to-10/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double Set, count, total;
+double[] History; 
+Set = Convert.ToDouble( Console.ReadLine());
+History
